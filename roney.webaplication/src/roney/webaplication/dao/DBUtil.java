73733f7319a9700c2ed424cd1d99ee9dbc9876f2
RONEY PEREIRA DO NASCIMENTO <54154161+RoneyPDN@users.noreply.github.com>
2019-10-10 14:51:05 -0300

    Add files via upload

--- roney.webaplication/src/roney/webaplication/dao/DBUtil.java
+++ roney.webaplication/src/roney/webaplication/dao/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
 	private static final String URL = "jdbc:mysql://localhost:3306/entregas";
 	private static final String USER = "root";
-	private static final String PASSWORD = "Nuneyboy!144";
+	private static final String PASSWORD = "senha";
 	private static DBUtil instancia;
 	private Connection con;
 	
